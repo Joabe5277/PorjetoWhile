85d9135cf116d2189472a18c01e7e4d560d24f39
Joabe5277 <54293241+Joabe5277@users.noreply.github.com>
2019-08-19 20:48:11 -0300

    Add files via upload

---
+++ PROJETO4- While/src/projeto4/pkgwhile/PROJETO4While.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto4.pkgwhile;
+
+/**
+ *
+ * @author aluno
+ */
+public class PROJETO4While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade =15;
+        while idade < 18){
+            System.out.println("Idade:" + idade);
+            idade = idade +1;
+        }
+        //de 0 a 10
+        int cont = 0;
+        while(cont <=10){
+            System.out.println("Valor de cont:"+ cont);
+            cont = cont +1;
+        }
+        cont = 10;
+        while(cont >=0){
+            System.out.println("Valor de cont:"+ cont);
+            cont = cont -1;
+     
+     
+        }
+        
+    }
+    
+    
+
